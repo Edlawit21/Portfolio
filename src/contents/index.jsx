135d4a2299bfc8f21745b8c06f579b5fbf39e410
Edlawit21 <edlawitbeyene21@gmail.com>
2024-11-15 17:59:07 +0300

    added project

--- src/contents/index.jsx
+++ src/contents/index.jsx
@@ -86,7 +86,7 @@ export const PROJECTS = [
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "TailwindCSS", "Laravel"],
+    technologies: ["HTML", "CSS", "React", "TailwindCSS", "Laravel 11"],
     status: "Finished",
   },
 ];
